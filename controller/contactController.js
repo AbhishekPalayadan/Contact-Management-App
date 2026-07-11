7f4ba15adbde955669e8dabb9c90a8c5ba0d5976
Abhishek <abhishekpalayadan20@gmail.com>
2026-07-11 21:59:52 +0530

    feat: set up initial Express application structure

---
+++ controller/contactController.js
@@ -0,0 +1,34 @@
+const getContacts=(req,res)=>{
+    res.status(200).json({message:"Get all contacts"});
+};
+
+const createContact=(req,res)=>{
+    console.log(req.body);
+    const {name,email,phone}=req.body;
+    if(!name || !email || !phone){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    res.status(201).json({message:"Create contact"});
+};
+
+const getContact=(req,res)=>{
+    res.status(200).json({message:`Get contacts for ${req.params.id}`});
+};
+
+const updateContact=(req,res)=>{
+    res.status(200).json({message:`Update contact for ${req.params.id}`});
+};
+
+const deleteContact=(req,res)=>{
+    res.status(200).json({message:`Delete contact for ${req.params.id}`})
+};
+
+
+module.exports={
+    getContacts,
+    createContact,
+    getContact,
+    updateContact,
+    deleteContact
+}
---
+++ server.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const dotenv=require('dotenv').config();
+const contactRoutes=require('./routes/contactRoutes')
+const app=express();
+
+const port=process.env.PORT || 5000;
+
+app.use(express.json())
+
+app.use('/api/contacts',contactRoutes);
+
+app.listen(3000,()=>{
+    console.log(`Server is running of ${port}`)
+})
